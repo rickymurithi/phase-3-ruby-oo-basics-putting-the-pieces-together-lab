836b6486bee979ecd4f1879147db5ed0873e74c6
erickmuriithi <erick.muriithi@student.moringaschool.com>
2022-10-23 09:10:33 +0300

    add file

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,18 @@
+#Book class.
+class Book
+    attr_writer:author,:page_count,:genre
+    attr_reader:author,:page_count,:genre
 
+    def initialize(title)
+        @title = title
+    end
+
+    def title
+        @title
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,17 @@
 # Make your shoe class here!
+class Shoe
+
+    attr_reader:brand,:color,:size,:material,:condition
+    attr_writer:brand,:color,:size,:material,:condition
+
+    def initialize(brand)
+        @brand = brand
+    end
+    def brand
+        @brand
+    end
+    def cobble
+        @condition = "new"
+        puts "Your shoe is as good as new!"
+    end
+end
